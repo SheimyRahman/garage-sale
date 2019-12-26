acc7e8b58079529449164645d0d03233051ff48c
Sheimy Rahman <33188116+SheimyRahman@users.noreply.github.com>
2019-12-25 23:21:59 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 angular.module('app', ['ngResource','ngRoute','ngSanitize'])
 .config(['$routeProvider',function($routeProvider) {
     $routeProvider
-        .when('/home',  { templateUrl: '/garage-store/templates/home.html', controller: 'homeCtrl'})
-        .when('/about',  { templateUrl: '/garage-store/templates/about.html', controller: 'emptyCtrl'})
+        .when('/home',  { templateUrl: '/garage-sale/templates/home.html', controller: 'homeCtrl'})
+        .when('/about',  { templateUrl: '/garage-sale/templates/about.html', controller: 'emptyCtrl'})
         .otherwise({redirectTo: '/home'});
 }])
 .factory('Items', ['$resource',function($resource){
-    return $resource("/garage-store/json/items.json");
+    return $resource("/garage-sale/json/items.json");
 }])
 .controller('homeCtrl', function($scope,Items,$routeParams){
     $scope.search = {vendido:false};
